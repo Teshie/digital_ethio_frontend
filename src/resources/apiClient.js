277a61f6fbd130095386d920dad5b197d466d65a
Teshie <teshomeaychiluhem@gmail.com>
2022-11-25 02:30:20 -0800

    base url changed

--- src/resources/apiClient.js
+++ src/resources/apiClient.js
@@ -1,6 +1,6 @@
 import http from "./http";
 
 // const localURL = "http://127.0.0.1:8000/api";
-const developmentUrl = "http://127.0.0.1:8000/api";
+const developmentUrl = "http://backend.erp.com.yayatourandtravels.com/api";
 
 export const baseURL = developmentUrl;
